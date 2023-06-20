01bf383c12d3d78c9673fe5aac8590c833a495f9
Deniz <77787415+deniz244@users.noreply.github.com>
2023-06-20 13:44:40 +0300

    1 event yapıldı

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
 // document.querySelector("h1").addEventListener("click",function(e){
 // alert("Bana tıkladın!")
 // });
 
-
 // Kodlar buraya gelecek!
+
+//mouseover
+const renkli = document.getElementsByClassName("nav-link");
+
+for (let i = 0; i < renkli.length; i++) {
+  renkli[i].addEventListener(
+    "mouseenter",
+    (evnt) => {
+      evnt.target.style.color = "orange";
+
+      setTimeout(() => {
+        evnt.target.style.color = "";
+      }, 600);
+    },
+    false
+  );
+}
